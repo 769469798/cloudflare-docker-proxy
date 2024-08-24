4f760d59288ba3e47108887599d1bec8eb7e48b9
769469798 <65400018+769469798@users.noreply.github.com>
2024-08-24 10:45:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const routes = {
   "ecr.769469798.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.769469798.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
